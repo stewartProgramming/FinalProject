7979cdedebc45f888394438d7625c8e008dc8ede
synthrootcoding <synthrootcoding@gmail.com>
2020-12-06 15:15:28 -0500

    first commit / connection

---
+++ FinalProject/Models/FootballDAL.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    public class FootballDAL
+    {
+        public static string CallTeamAPI()
+        {
+            string url = $"https://raw.githubusercontent.com/openfootball/football.json/master/2019-20/cl.clubs.json";
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string output = rd.ReadToEnd();
+            return output;
+        }
+
+        public static List<Club> GetTeams()
+        {
+            string data = CallTeamAPI();
+            Rootobject r = JsonConvert.DeserializeObject<Rootobject>(data);
+            List<Club> clubs = r.clubs.ToList();
+            return clubs;
+        }
+    }
+}
---
+++ FinalProject/Models/FootballTeams.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    public class Rootobject
+    {
+        public string name { get; set; }
+        public List<Club> clubs { get; set; }
+    }
+
+    public class Club
+    {
+        public string name { get; set; }
+        public string code { get; set; }
+        public string country { get; set; }
+    }
+}
