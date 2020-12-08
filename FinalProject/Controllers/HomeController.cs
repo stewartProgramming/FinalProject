d6515ed2b851c9a8609d15c14830c0804ec07fbf
Lindsay <lindsaynadell89@gmail.com>
2020-12-08 12:03:37 -0500

    updated navigation

--- FinalProject/Controllers/HomeController.cs
+++ FinalProject/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using FinalProject.Models;
+using FinalProject.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -12,10 +13,12 @@ namespace FinalProject.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly HighlightService _highlightService;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _highlightService = new HighlightService();
         }
 
         public IActionResult Index()
@@ -23,44 +26,17 @@ namespace FinalProject.Controllers
             return View();
         }
 
-        //break Highlight list into lists of 10
-        public static List<List<Highlight>> SplitList(List<Highlight> highlights)
-        {
-            var list = new List<List<Highlight>>();
-
-            for (int i = 0; i < highlights.Count; i += 10)
-            {
-                list.Add(highlights.GetRange(i, Math.Min(10, highlights.Count - i)));
-            }
-            return list;
-        }
-        //figure out what page to display
-        //[HttpGet]
+        //figure out what list to display
         public IActionResult RecentHighlights(int? page)
         {
-            List<Highlight> highlights = FootballDAL.GetHighlights();
-            List<List<Highlight>> list = SplitList(highlights).ToList();
+            List<List<Highlight>> list = _highlightService.GetHighlights();
             if (page == null)
             {
                 page = 1;
             }
             ViewBag.pageCount = page;
-
+            ViewBag.listCount = list.Count;
             return View(list[(int)page - 1]);
-            //switch (page)
-            //{
-            //    case 1:
-            //        return View(list[0]);
-            //    case 2:
-            //        return View(list[1]);
-            //    case 3:
-            //        return View(list[2]);
-            //    case 4:
-            //        return View(list[3]);
-            //    case 5:
-            //        return View(list[4]);
-            //}
-            //return View();
         }
 
         [HttpPost]
